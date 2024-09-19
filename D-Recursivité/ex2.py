ea7cd1a63c1032da679765b621890722e799142d
0xb0c4n <78223266+0xb0c4n@users.noreply.github.com>
2024-09-19 10:34:38 +0200

    Update ex2.py

--- D-Recursivité/ex2.py
+++ D-Recursivité/ex2.py
@@ -2,8 +2,9 @@ import sys
 sys.setrecursionlimit(2000)
 
 def syracuse(un):
-	if un % 2 == 0:
+	while un > 1:
+	   if un % 2 == 0:
 		print(syracuse(un)/2)
-	else:
+	   else:
 		print(syracuse(un)*3+1)
 syracuse(10)

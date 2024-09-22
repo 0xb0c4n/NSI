c25e67df0fd401e2e1b0898b3a41d72f5ab8d44d
0xb0c4n <womercode@gmail.com>
2024-09-22 23:13:57 +0200

    Fin exos

--- D-Recursivité/ex8.py
+++ D-Recursivité/ex8.py
@@ -5,8 +5,12 @@ def koch(n,l):
 	if n == 0:
 		t.forward(l)
 	else:
-		koch(n-1,l)
+		koch(n-1, l)
 		t.left(60)
+		koch(n-1, l)
+		t.right(120)		
+		koch(n-1, l)
 		t.left(60)
-		koch(n-1,l)
-		
+		koch(n-1, l)
+
+koch(50,10)

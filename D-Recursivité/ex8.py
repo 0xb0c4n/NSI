e8b8a024da6ef4391622d893c543d9c421075e81
0xb0c4n <78223266+0xb0c4n@users.noreply.github.com>
2024-09-23 16:20:31 +0200

    Update ex8.py

--- D-Recursivité/ex8.py
+++ D-Recursivité/ex8.py
@@ -5,12 +5,12 @@ def koch(n,l):
 	if n == 0:
 		t.forward(l)
 	else:
-		koch(n-1, l)
+		koch(n-1, l/3)
 		t.left(60)
-		koch(n-1, l)
+		koch(n-1, l/3) 
 		t.right(120)		
-		koch(n-1, l)
+		koch(n-1, l/3)
 		t.left(60)
-		koch(n-1, l)
+		koch(n-1, l/3)
 
 koch(50,10)

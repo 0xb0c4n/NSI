365c45047a3c65b4adbbaff9f1a31bc81249a337
0xb0c4n <womercode@gmail.com>
2024-10-07 17:25:10 +0200

    POO

--- poo/ex3.py
+++ poo/ex3.py
@@ -2,3 +2,36 @@ class Intervalle:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+    def est_vide(self):
+        return self.b < self.a
+
+    def __len__(self):
+        return 0 if self.est_vide() else self.b - self.a
+    
+    def __contains__(self, x):
+        return self.a <= x <= self.b
+
+    def __eq__(self, i2):
+        return self.est_vide() and i2.est_vide() or self.a == i2.a and self.b == i2.b 
+    
+    def __le__(self, i2):
+        return i2.est_vide() and self.est_vide() or self.a >= i2.a and self.b <= i2.b
+    
+    def intersection(self, i2):
+        if i2.a <= self.b and i2.a >= self.a:
+            return Intervalle(i2.a, self.b)
+        elif self <= i2:
+            return Intervalle(i2.a,i2.b)
+        elif self == i2:
+            return Intervalle(self.a, self.b)
+        else:
+            return Intervalle(i2.b, self.a)
+        
+    def union(self, i2):
+        if not i2.est_vide() and not self.est_vide():
+            return Intervalle(min(self.a, i2.a), max(self.b, i2.b))
+        else:
+            return Intervalle(1,0)            
+    
+    
---
+++ poo/ex4.py
@@ -0,0 +1,23 @@
+import math
+
+class Angle:
+    def __init__(self, angle):
+        if angle < 0 or angle > 360:
+            raise ValueError
+        self.angle = angle
+
+    def __str__(self):
+        return str(self.angle) + " degrés"
+        
+    def ajoute(self, Angle2):
+        calcul = self.angle + Angle2.angle
+        if calcul > 360:
+            self.angle = calcul % 360
+        else:
+            self.angle = calcul
+
+    def cos(self):
+        math.cos(self.angle) * (math.pi / 180)
+
+    def sin(self):
+        math.sin(self.angle) * (math.pi / 180)

013de2393d8979367e9c6f9c344e60bc01880b84
0xb0c4n <womercode@gmail.com>
2024-11-18 20:37:21 +0100

    Fin

--- stack/ex3.py
+++ stack/ex3.py
@@ -1,20 +1,19 @@
 from Pile import *
-from File import *
 
 def parenthese_ferme(string, index):
-    p = creer_file()
+    p = creer_pile()
     for i in range(len(string)):
         if index >= len(string):
             return None
         elif i == index:
-            precedent = p.retirer()
-            if precedent['char'] == '(':
-                return precedent['index']
+            precedent = p.depiler()
+            if precedent:
+                return precedent
             else:
                 return None
         else:
             if string[i] in '()':
-                p.ajouter({"char": string[i], "index": i})
+                p.empiler(i)
 
 print(parenthese_ferme("zet(gtrt()())0",9))
 print(parenthese_ferme("zet(gtrt()())0",2))

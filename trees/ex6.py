b84831bd5786fdbe94674acab2fa71bd4a5c2f00
0xb0c4n <78223266+0xb0c4n@users.noreply.github.com>
2024-11-28 11:33:35 +0100

    Update ex6.py

--- trees/ex6.py
+++ trees/ex6.py
@@ -5,7 +5,7 @@ def parfait(h):
     if h == 0:
         return None
     else:
-        return Noeud(parfait(h-1), 2**(h-1), parfait(h-1))
+        return Noeud(parfait(h-1), h, parfait(h-1))
     
 print(str_arbre(parfait(6)))
 print(taille(parfait(6))==2**6 - 1)

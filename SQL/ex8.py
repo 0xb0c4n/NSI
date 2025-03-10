9e4f063e418a1eb8dfa4264c76781036eb32ba6c
0xb0c4n <womercode@gmail.com>
2025-03-10 16:59:59 +0100

    POTD

---
+++ SQL/ex8.py
@@ -0,0 +1,26 @@
+import sqlite3 as sgbd
+from datetime import date, timedelta
+class Usager:
+    def __init__(self, code_barre):
+        self.cnx= sgbd.connect('/home/tnsi-eleve5/mediatheque.db')
+        self.c = self.cnx.cursor()
+        self.code_barre = code_barre
+
+    def __del__(self):
+        self.cnx.close()
+
+    def emprunter(self, isbn):
+        self.c.execute('INSERT INTO emprunt VALUES (?,?,?)', [self.code_barre, isbn, date.today()+timedelta(days=30)])
+
+    def rendre(self, isbn):
+        self.c.execute('DELETE FROM emprunt WHERE isbn = ?', [isbn])
+        
+    def emprunts(self):
+        self.c.execute('SELECT * FROM emprunt WHERE code_barre = ?', [self.code_barre])
+        return self.c.fetchall()
+
+u = Usager("137332830764072")
+print(u.emprunts())
+u.rendre('978-2013230827')
+u.emprunter('978-0132762564')
+print(u.emprunts())

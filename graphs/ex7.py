35940f3221c17488285fc5689c52e8bdf9390377
0xb0c4n <womercode@gmail.com>
2025-05-13 09:16:09 +0200

    POTD7

--- graphs/ex7.py
+++ graphs/ex7.py
@@ -1,7 +1,40 @@
-def largeur(a, s):
-	dist = {s: 0}
-	courant = {s}
+class Noeud:
+    def __init__(self,g,v,d):
+        self.gauche = g
+        self.valeur = v
+        self.droite = d
+
+def parfait(h):
+    if h == 0:
+        return None
+    else:
+        return Noeud(parfait(h-1), h, parfait(h-1))
+	
+arbre = parfait(5)
+
+def largeur(a):
+	dist = {a.valeur: 0}
+	courant = {a}
 	suivant = set()
 	while len(courant) > 0:
 		s = courant.pop()
-		for v in [a.gauche, a.droite]:
+		for v in [s.gauche, s.droite]:
+			if v!= None and v.valeur not in dist:
+				suivant.add(v)
+				dist[v.valeur] = dist[s.valeur] + 1
+		if len(courant) == 0:
+			courant, suivant = suivant, set()
+	return dist
+
+def distance(g, u, v):
+	dist = largeur(g, u)
+	return dist[v] if v in dist else None
+
+
+def str_arbre(a):
+    if a is None:
+        return ""
+    else:
+        return "(" + str_arbre(a.gauche) + str(a.valeur) + str_arbre(a.droite) + ")"
+
+print(largeur(arbre))
